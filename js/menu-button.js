d184083155bdf3d4328e127e94567626db6867a9
Sofia Protsiv <prochivsofia@gmail.com>
2021-08-10 20:19:20 +0300

    menu-nav is ready

---
+++ js/menu-button.js
@@ -0,0 +1,15 @@
+
+    (() =>{
+        const menuBtmRef = document.querySelector("[data-menu-button]");
+        const mobilMenuRef = document.querySelector("[data-menu]");
+
+        menuBtmRef.addEventListener("click", () =>{
+            const expanded = menuBtmRef.getAttribute("aria-expanded") === "true" || false;
+
+
+            menuBtmRef.classList.toggle("menu__button--open");
+            menuBtmRef.setAttribute("aria-expanded", !expanded);
+
+            mobilMenuRef.classList.toggle("nav-menu--open");
+        });
+    })();
