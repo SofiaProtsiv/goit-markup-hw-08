2cdeb93ca64508d4d8372d8d15434929f4b0f6c1
Sofia Protsiv <prochivsofia@gmail.com>
2021-08-12 21:55:55 +0300

    .

--- js/modal.js
+++ js/modal.js
@@ -7,5 +7,6 @@ refs.openModalBtn.addEventListener("click", toggleModal);
 refs.closeModalBtn.addEventListener("click", toggleModal);
 
 function toggleModal() {
+    document.body.classList.toggle("modal-open")
     refs.backdrop.classList.toggle("backdrop--hidden");
 }
