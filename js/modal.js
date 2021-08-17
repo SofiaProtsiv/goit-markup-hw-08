f705fd4179988acb0f61aa0f4d7d89fd728de717
Sofia Protsiv <prochivsofia@gmail.com>
2021-08-17 10:11:00 +0300

    змінено nav-menu

--- js/modal.js
+++ js/modal.js
@@ -7,6 +7,6 @@ refs.openModalBtn.addEventListener("click", toggleModal);
 refs.closeModalBtn.addEventListener("click", toggleModal);
 
 function toggleModal() {
-    document.body.classList.toggle("modal-open")
+    document.body.classList.toggle("backdrop")
     refs.backdrop.classList.toggle("backdrop--hidden");
 }
